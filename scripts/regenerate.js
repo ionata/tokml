061d8321104b9c83e9f54c6cdd81e41cb5655f78
Evo Stamatov <aviolito@gmail.com>
2023-11-25 00:52:23 +1100

    feat: add options to be read when regenerating kml test files

--- scripts/regenerate.js
+++ scripts/regenerate.js
@@ -1,7 +1,18 @@
 import fs from 'node:fs';
+import path from 'node:path';
 import glob from 'glob';
 import tokml from '../lib/index.js';
 
-glob.sync('test/data/*.geojson').forEach(function (g) {
-  fs.writeFileSync(g.replace('.geojson', '.kml'), tokml(JSON.parse(fs.readFileSync(g))));
+glob.sync('test/data/*.geojson').forEach((geoFile) => {
+  const kmlFile = geoFile.replace('.geojson', '.kml');
+  const optionsFile = geoFile.replace('.geojson', '.options.json');
+  let options;
+  try {
+    options = JSON.parse(fs.readFileSync(optionsFile));
+  } catch (err) {
+    if (err.code !== 'ENOENT') throw err;
+    if (path.basename(geoFile).startsWith('simplestyle_')) options = { simplestyle: true };
+  }
+  const output = tokml(JSON.parse(fs.readFileSync(geoFile)), options);
+  fs.writeFileSync(kmlFile, output);
 });
